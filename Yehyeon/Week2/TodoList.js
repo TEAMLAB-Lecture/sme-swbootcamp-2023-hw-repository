b18afe178a3bed94d6c07342d6faa6c808a82a42
AnYeHyeon <ahnyh4756@gmail.com>
2023-07-16 16:43:16 +0900

    add ToDoList.png

--- Yehyeon/Week2/TodoList.js
+++ Yehyeon/Week2/TodoList.js
@@ -7,7 +7,7 @@ function addTask() {
     if (taskText.trim() !== "") {
         var newTask = document.createElement("ul");
         newTask.setAttribute("class", "task")
-        newTask.innerHTML = `<span id="taskbox"><input type="checkbox" name="checkbox"><input type="text" value="${taskInput.value}" readOnly></input></span>`;
+        newTask.innerHTML = `<span id="taskbox"><input type="checkbox" name="checkbox"><input type="text" name="task" value="${taskInput.value}" readOnly></input></span>`;
         newTask.innerHTML += `<span id="save"><input type="button" class="modisavebtns" value="수정" onclick="readOnlyFalse(this)"><input type="button" class="modisavebtns" value="저장" onclick="readOnlyTrue(this)"></span>`;
         // newTask.innerText = taskText;
         taskList.appendChild(newTask);
@@ -15,6 +15,14 @@ function addTask() {
     }   else {
             alert('Input the task.')
     }
+}
+
+function readOnlyTrue(e){
+    e.parentNode.previousSibling.lastChild.readOnly=true;
+}
+
+function readOnlyFalse(e){
+    e.parentNode.previousSibling.lastChild.readOnly=false;
 }
 
 // 엔터키로도 입력되도록 함.
@@ -25,3 +33,10 @@ taskInput.addEventListener("keydown", function(event) {
     addTask();
   }
 });
+
+function selectAllTasks() {
+    const checkboxes = document.getElementsByName('task');
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = true;
+    }
+}
